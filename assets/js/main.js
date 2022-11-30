d584ea91584e82d4e07d65c9093ca2214efa473f
The-Malicious <buddyhaldar85@gmail.com>
2022-12-01 00:56:28 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,44 @@
-$('.testimonial').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+$(document).ready(function () {
+    $('.testimonial').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
         }
-    }
-})
+    })
 
-$('.brannd-logo-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots: false,
-    responsive:{
-        0:{
-            items:5
-        },
-        600:{
-            items:5
-        },
-        1000:{
-            items:5
+    $('.brannd-logo-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 5
+            }
         }
-    }
+    })
+
+    $(".close-icon").click(function(){
+        $('.menu').hide(500)
+      });
+    $(".menu-icon").click(function(){
+        $('.menu').show(500)
+      });
+
 })
