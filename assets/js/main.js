3c0bbde3b180a489e3aa386d6765823f9ed8794c
The-Malicious <buddyhaldar85@gmail.com>
2022-11-30 21:16:45 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$('.testimonial').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.brannd-logo-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:5
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
